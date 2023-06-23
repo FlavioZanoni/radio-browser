3070e97a7589faa3c646da8a9fe53afbe7596345
FlavioZanoni <flaviozg888@gmail.com>
2023-06-23 00:59:25 -0300

    Adicionando ediçao de radio

--- src/components/RadioCard/RadioCard.tsx
+++ src/components/RadioCard/RadioCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { RadioStation } from "../../lib/api/types"
 
 type Props = {
@@ -11,6 +12,9 @@ export const RadioCard: React.FC<Props> = ({
   station,
   setFavorites,
 }) => {
+  const [isEditing, setIsEditing] = useState(false)
+  const [newName, setNewName] = useState(station.name)
+
   return (
     <div className="flex flex-row w-full items-center justify-between bg-slate-200 rounded-md p-2 gap-2">
       <div className="flex gap-4">
@@ -22,25 +26,65 @@ export const RadioCard: React.FC<Props> = ({
           <img className="w-6" src="/icons/play.svg" alt="play" />
         </button>
         <div className="flex flex-col">
-          <p>{station.name}</p>
-          <p className="text-xs">
-            {station.country} {station.tags && " - " + station.tags}
-          </p>
+          {isEditing ? (
+            <input
+              className="rounded-sm w-full"
+              type="text"
+              value={newName}
+              onChange={(e) => {
+                setNewName(e.target.value)
+              }}
+            />
+          ) : (
+            <>
+              <p>{station.name}</p>
+              <p className="text-xs">
+                {station.country} {station.tags && " - " + station.tags}
+              </p>
+            </>
+          )}
         </div>
       </div>
       <div>
-        <button
-          onClick={() => {
-            setFavorites((prev) =>
-              prev.filter(
-                (prevStation) => prevStation.stationuuid !== station.stationuuid
+        {isEditing ? (
+          <button
+            onClick={() => {
+              setFavorites((prev) =>
+                prev.map((prevStation) =>
+                  prevStation.stationuuid === station.stationuuid
+                    ? { ...prevStation, name: newName }
+                    : prevStation
+                )
               )
-            )
-          }}
-        >
-          <img className="w-6" src="/icons/trash.svg" alt="excluir" />
-        </button>
-        <button>{/* icone de editar */}</button>
+
+              setIsEditing(false)
+            }}
+          >
+            <img className="w-6" src="/icons/check.svg" alt="salvar" />
+          </button>
+        ) : (
+          <div className="flex gap-2">
+            <button
+              onClick={() => {
+                setIsEditing(true)
+              }}
+            >
+              <img className="w-6" src="/icons/edit.svg" alt="editar" />
+            </button>
+            <button
+              onClick={() => {
+                setFavorites((prev) =>
+                  prev.filter(
+                    (prevStation) =>
+                      prevStation.stationuuid !== station.stationuuid
+                  )
+                )
+              }}
+            >
+              <img className="w-6" src="/icons/trash.svg" alt="excluir" />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   )
